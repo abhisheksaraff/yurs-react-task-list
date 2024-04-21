1c671eac32e6f4d747fdd8515111da2372e66c4b
a6hi <saraffabhishek@outlook.com>
2024-04-21 12:07:32 -0400

    setup NavBar

--- src/TaskList/NavBar.js
+++ src/TaskList/NavBar.js
@@ -3,7 +3,12 @@ import { Link } from "react-router-dom";
 function NavBar() {
   return (
     <div className = "NavBar">
-      NavBar
+      Logo
+      <div className = "navigation-buttons">
+        <Link to="/">Home</Link>
+        <Link to="/CompletedTasks">Completed Tasks</Link>
+        <Link to="/TodoTasks">Todo Tasks</Link>
+      </div>
     </div>
   )
 };
